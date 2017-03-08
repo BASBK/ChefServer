cd58092f892add6cee44d91260c791a0f028b395
basbk <basbkk1@gmail.com>
2017-03-08 19:05:24 +0300

    Swap to PonyORM. Looks great now

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
-from playhouse.shortcuts import *
+from pony.orm.serialization import to_dict, to_json
 from models import *
-from datetime import date
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -12,26 +12,38 @@ def helloworld():
 
 
 @app.route('/api/populate')
+@db_session
 def populate():
-    db.create_tables([DeliveryType, Delivery, Menu, Address, Client, Order, OrderMenuThrough], safe=True)
-    dt1 = DeliveryType.create(type_name='Пиццерия')
-    d1 = Delivery.create(name='Ташир', type=dt1, courier_chatID='@courier', cook_chatID='@cooking')
-    m1 = Menu.create(delivery_name=d1, name='4 сыра', description='Вкуснота', weight=1500, price=650)
-    m2 = Menu.create(delivery_name=d1, name='Мясная', description='Ваще оч вкусно', weight=1600, price=750)
-    a1 = Address.create(latitude=58.264846, longitude=65.211548, additional_info='подъезд 2, кв. 348, этаж 16')
-    c1 = Client.create(chatID='@client', address=a1)
-    o1 = Order.create(date=date.today(), count=2, client=c1)
-    o2 = Order.create(date=date.today(), count=3, client=c1)
-    o1.menu_position.add(o1)
+    dt1 = DeliveryType(type_name='Пиццерия')
+    d1 = Delivery(name='Ташир', type=dt1, courier_chatID='@courier', cook_chatID='@cooking')
+    m1 = Menu(delivery_name=d1, name='4 сыра', description='Вкуснота', weight=1500, price=650)
+    m2 = Menu(delivery_name=d1, name='Мясная', description='Ваще оч вкусно', weight=1600, price=750)
+    a1 = Address(latitude=58.264846, longitude=65.211548, additional_info='подъезд 2, кв. 348, этаж 16')
+    c1 = Client(chatID='@client', address=a1)
+    oi1 = OrderInfo(client=c1)
+    Order(number=oi1, date=datetime.now(), menu_position=m1, count=2)
+    Order(number=oi1, date=datetime.now(), menu_position=m2, count=1)
+    oi2 = OrderInfo(client=c1)
+    Order(number=oi2, date=datetime.now(), menu_position=m2, count=3)
     return 'success'
 
 
 @app.route('/api/orders')
+@db_session
 def orders():
     orders = []
-    [orders.append(model_to_dict(o, recurse=False)) for o in Order.select()]
+    for o in OrderInfo.select():
+        orders.append(o.to_dict(with_collections=True))
     return jsonify(orders)
 
+
+@app.route('/api/orders_info')
+@db_session
+def orders_info():
+    orders = []
+    for o in Order.select():
+        orders.append(o.to_dict(with_collections=True))
+    return jsonify(orders)
 
 
 @app.route('/api/orders/<int:number>')
--- models.py
+++ models.py
@@ -1,52 +1,57 @@
-from peewee import *
-from playhouse.fields import *
+from pony.orm import *
+from datetime import datetime
 
-db = SqliteDatabase('chef_db.sqlite')
-
-
-class BaseModel(Model):
-    class Meta:
-        database = db
+db = Database()
 
 
-class DeliveryType(BaseModel):
-    type_name = CharField(primary_key=True)
+class DeliveryType(db.Entity):
+    type_name = PrimaryKey(str)
+    delivery = Optional('Delivery')
 
 
-class Delivery(BaseModel):
-    name = CharField(primary_key=True)
-    type = ForeignKeyField(DeliveryType, related_name='delivery')
-    courier_chatID = CharField()
-    cook_chatID = CharField()
+class Delivery(db.Entity):
+    name = PrimaryKey(str)
+    type = Required(DeliveryType)
+    courier_chatID = Required(str)
+    cook_chatID = Required(str)
+    menu = Set('Menu')
 
 
-class Address(BaseModel):
-    latitude = DoubleField()
-    longitude = DoubleField()
-    additional_info = TextField()
+class Address(db.Entity):
+    latitude = Required(float)
+    longitude = Required(float)
+    additional_info = Required(str)
+    client = Optional('Client')
 
 
-class Client(BaseModel):
-    chatID = CharField(primary_key=True)
-    address = ForeignKeyField(Address, related_name='client')
+class Client(db.Entity):
+    chatID = PrimaryKey(str)
+    address = Required(Address)
+    order = Set('OrderInfo')
 
 
-class Order(BaseModel):
-    number = IntegerField(primary_key=True)
-    date = DateTimeField
-    #menu_position = ManyToManyField(Menu, related_name='order')
-    count = IntegerField()
-    client = ForeignKeyField(Client, related_name='order')
+class Order(db.Entity):
+    number = Required('OrderInfo')
+    date = Required(datetime)
+    menu_position = Required('Menu')
+    count = Required(int)
 
 
-class Menu(BaseModel):
-    delivery_name = ForeignKeyField(Delivery, related_name='menu')
-    name = CharField()
-    description = TextField()
-    weight = DoubleField()
-    price = DoubleField()
-    photo = CharField(default='none')
-    order = ManyToManyField(Order, related_name='menu_position')
+class Menu(db.Entity):
+    delivery_name = Required(Delivery)
+    name = Required(str)
+    description = Required(str)
+    weight = Required(float)
+    price = Required(float)
+    photo = Optional(str)
+    order = Set(Order)
 
 
-OrderMenuThrough = Menu.order.get_through_model()
+class OrderInfo(db.Entity):
+    number = PrimaryKey(int, auto=True)
+    client = Required(Client)
+    order = Set(Order)
+
+
+db.bind('sqlite', 'chef.db', create_db=True)
+db.generate_mapping(create_tables=True)
